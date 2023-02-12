545fe171315af6f1c6da4d9a9adb1ae6066e1c53
Aarohi2110 <82254142+Aarohi2110@users.noreply.github.com>
2023-02-12 19:52:46 +0530

    Update 433. Minimum Genetic Mutation.cpp

--- 433. Minimum Genetic Mutation.cpp
+++ 433. Minimum Genetic Mutation.cpp
@@ -1,3 +1,4 @@
+//BFS traversal
 class Solution {
  public:
   int minMutation(string start, string end, vector<string>& bank) {
@@ -5,30 +6,30 @@ class Solution {
     if (!bankSet.count(end))
       return -1;
 
-    int ans = 0;
-    queue<string> q{{start}};
-
+    queue<pair<string,int>> q{{make_pair(start,0)}};
+    vector<string> visited={start};
     while (!q.empty()) {
-      ++ans;
-      for (int sz = q.size(); sz > 0; --sz) {
-        string word = q.front();
-        q.pop();
+        string word = q.front().first;
+        int level=q.front().second;
+        //cout<<word<<level;
+            q.pop();
+        if (word == end)
+             return level; 
         for (int j = 0; j < word.length(); ++j) {
-          const char cache = word[j];
+          string cache = word;
           for (const char c : {'A', 'C', 'G', 'T'}) {
-            word[j] = c;
-            if (word == end)
-              return ans;
-            if (bankSet.count(word)) {
-              bankSet.erase(word);
-              q.push(word);
+            cache[j] = c;
+    if (bankSet.find(cache)!=bankSet.end() && find(visited.begin(),visited.end(),cache)==visited.end()){
+              q.push(make_pair(cache,level+1));
+              visited.push_back(cache);
             }
           }
-          word[j] = cache;
-        }
-      }
+          
     }
+     }
 
     return -1;
   }
 };
+
+    
